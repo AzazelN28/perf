d69283764855c7fa133fb08fca1086beb1736b12
AzazelN28 <asakon28@gmail.com>
2023-06-02 13:17:16 +0200

    test: add more tests

--- test-wasm-plain.mjs
+++ test-wasm-plain.mjs
@@ -43,7 +43,118 @@ class Rect {
   }
 }
 
+class RectFloat32 extends Float32Array {
+  static random(min, max) {
+    return new RectFloat32(
+      random(min, max),
+      random(min, max),
+      random(min, max),
+      random(min, max)
+    )
+  }
+
+  constructor(x = 0, y = 0, width = 0, height = 0) {
+    super([x, y, width, height])
+  }
+
+  get x() {
+    return this[0]
+  }
+
+  set x(v) {
+    this[0] = v
+  }
+
+  get y() {
+    return this[1]
+  }
+
+  set y(v) {
+    this[1] = v
+  }
+
+  get width() {
+    return this[2]
+  }
+
+  set width(v) {
+    this[2] = v
+  }
+
+  get height() {
+    return this[3]
+  }
+
+  set height(v) {
+    this[3] = v
+  }
+
+  contains({ x, y }) {
+    return x >= this.x
+      && y >= this.y
+      && x <= this.x + this.width
+      && y <= this.y + this.height
+  }
+}
+
+class RectFloat32Composed {
+  static random(min, max) {
+    return new RectFloat32Composed(
+      random(min, max),
+      random(min, max),
+      random(min, max),
+      random(min, max)
+    )
+  }
+
+  constructor(x = 0, y = 0, width = 0, height = 0) {
+    this.data = new Float32Array([
+      x, y, width, height
+    ])
+  }
+
+  get x() {
+    return this.data[0]
+  }
+
+  set x(v) {
+    this.data[0] = v
+  }
+
+  get y() {
+    return this.data[1]
+  }
+
+  set y(v) {
+    this.data[1] = v
+  }
+
+  get width() {
+    return this.data[2]
+  }
+
+  set width(v) {
+    this.data[2] = v
+  }
+
+  get height() {
+    return this.data[3]
+  }
+
+  set height(v) {
+    this.data[3] = v
+  }
+
+  contains({ x, y }) {
+    return x >= this.x
+      && y >= this.y
+      && x <= this.x + this.width
+      && y <= this.y + this.height
+  }
+}
+
 class RectPointer {
+
   constructor(memory, offset) {
     this.memory = memory
     this.offset = offset
@@ -117,8 +228,12 @@ async function main() {
    * Initialization
    */
   const sparse = new Array(size)
+  const sparseFloat = new Array(size)
+  const sparseFloatComposed = new Array(size)
   for (let i = 0; i < size; i++) {
     sparse[i] = Rect.random(-1000, 1000)
+    sparseFloat[i] = RectFloat32.random(-1000, 1000)
+    sparseFloatComposed[i] = RectFloat32Composed.random(-1000, 1000)
   }
 
   const compact = new Float32Array(size * 4)
@@ -153,14 +268,32 @@ async function main() {
     performance.mark('instance:end')
     const measure2 = performance.measure('instance', 'instance:start', 'instance:end')
 
-    const min = Math.min(measure0.duration, measure1.duration, measure2.duration)
+    performance.mark('instance-float32:start')
+    for (let i = 0; i < size; i++) {
+      sparseFloat[i].contains(point)
+    }
+    performance.mark('instance-float32:end')
+    const measure3 = performance.measure('instance-float32', 'instance-float32:start', 'instance-float32:end')
+
+    performance.mark('instance-float32-composed:start')
+    for (let i = 0; i < size; i++) {
+      sparseFloatComposed[i].contains(point)
+    }
+    performance.mark('instance-float32-composed:end')
+    const measure4 = performance.measure('instance-float32-composed', 'instance-float32-composed:start', 'instance-float32-composed:end')
+
+    const min = Math.min(measure0.duration, measure1.duration, measure2.duration, measure3.duration, measure4.duration)
 
     const color0 = measure0.duration === min ? '\x1B[0;32m' : '\x1B[0;31m'
     const color1 = measure1.duration === min ? '\x1B[0;32m' : '\x1B[0;31m'
     const color2 = measure2.duration === min ? '\x1B[0;32m' : '\x1B[0;31m'
+    const color3 = measure3.duration === min ? '\x1B[0;32m' : '\x1B[0;31m'
+    const color4 = measure4.duration === min ? '\x1B[0;32m' : '\x1B[0;31m'
     console.log(`  ${color0}wasm\x1B[0;33m`, measure0.duration, '\x1B[0m')
     console.log(`  ${color1}pointer\x1B[0;33m`, measure1.duration, '\x1B[0m')
     console.log(`  ${color2}instance\x1B[0;33m`, measure2.duration, '\x1B[0m')
+    console.log(`  ${color3}instance-float32\x1B[0;33m`, measure3.duration, '\x1B[0m')
+    console.log(`  ${color4}instance-float32-composed\x1B[0;33m`, measure4.duration, '\x1B[0m')
   }
 }
 
